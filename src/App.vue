454a88e6e3499fd2def322b23f50fc2903217e84
VFryzlova <55710895+VFryzlova@users.noreply.github.com>
2020-08-06 15:52:13 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Alert v-bind:alertType="'successful'" v-bind:message="successMessage" />
+    <Alert v-bind:alertType="'confirmation'" v-bind:message="confirmationMessage" />
+    <Alert v-bind:alertType="'unsuccessful'" v-bind:message="failedMessage" />
+  </div>
+</template>
+
+<script>
+  import Alert from "./components/Alert.vue";
+  export default {
+    name: 'App',
+    components: {
+      Alert: Alert
+    },
+    data(){
+      return{
+        successMessage: "Požadavek byl potvrzen.",
+        confirmationMessage: "Potvrzení požadavku.",
+        failedMessage: "Požadavek nebyl potvrzen."
+      }
+    }
+  };
+</script>
+
+<style>
+  *{
+    box-sizing: border-box;
+  }
+  body{
+    font-family: 'Roboto', sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #E5E5E5;
+  }
+</style>
---
+++ src/components/Alert.vue
@@ -0,0 +1,62 @@
+<template>
+  <div v-bind:class="[{ successfulClass: alertType === 'successful', unsuccessfulClass: alertType === 'unsuccessful',   confirmationClass: alertType === 'confirmation' }, 'alert__box']">
+      <div v-bind:class="[{ successfulClassIcon: alertType === 'successful', unsuccessfulClassIcon: alertType === 'unsuccessful',   confirmationClassIcon: alertType === 'confirmation' }, 'icon']"></div>
+      <div class="message">{{ message }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Alert',
+    props: {
+      alertType: String,
+      message: String
+    }
+  };
+</script>
+
+<style>
+  .alert__box{
+    display: flex;
+    width: 60%;
+    margin: 30px auto;
+    padding: 30px;
+    border-radius: 5px;
+  }
+  .successfulClass{
+    background-color: #E9FFEE;
+    border-left: 5px solid #44B86A;
+    color: #408859;
+  }
+  .confirmationClass{
+    background-color: #E5E9F6;
+    border-left: 5px solid #60558D;
+    color: #160361;
+  }
+  .unsuccessfulClass{
+    background-color: #FFE9E9;
+    border-left: 5px solid #EE1B28;
+    color: #CF0D15;
+  }
+  .icon{
+    width: 50px;
+    height: 50px;
+  }
+  .successfulClassIcon{
+    background-image: url("../assets/img/success.png");
+    background-size: cover;
+  }
+  .confirmationClassIcon{
+    background-color: white;
+    border-radius: 50px;
+  }
+  .unsuccessfulClassIcon{
+    background-image: url("../assets/img/failed.png");
+    background-size: cover;
+  }
+  .message{
+    margin-left: 30px;
+    display: flex;
+    align-items: center;
+  }
+</style>
